c83b14b159a059af80119a9be8a8bfd33adc62b5
ari10mi.programing <ari10mi.programing@gmail.com>
2020-04-19 16:51:52 +0900

    initail signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/' => "home#top"
+  get '/signup' => "users#new"
+  post 'users/create' => "users#create"
+  root "home#top"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
